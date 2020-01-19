bf2b318f8ced05c96ca43badcb091e45a7517771
anupriyamranjit <41527609+anupriyamranjit@users.noreply.github.com>
2020-01-19 11:49:58 -0500

    Done
    :)

--- location/views.py
+++ location/views.py
@@ -13,7 +13,15 @@ def index(request):
     'Authorization': 'prj_live_sk_5bff60481af1adf8a3793ce4b3a12f48e3f424de',
     }
     event_details = requests.get('https://api.radar.io/v1/events', headers=privatekey).json()
-    lengthofEvents = len(event_details['events'])    
+    lengthofEvents = len(event_details['events'])
+    ID_DICT = {IDtoUser.objects.get(id=1).ID_Device:IDtoUser.objects.get(id=1).name , 
+        IDtoUser.objects.get(id=2).ID_Device:IDtoUser.objects.get(id=2).name,
+        IDtoUser.objects.get(id=3).ID_Device:IDtoUser.objects.get(id=3).name,
+        IDtoUser.objects.get(id=4).ID_Device:IDtoUser.objects.get(id=4).name,
+        IDtoUser.objects.get(id=5).ID_Device:IDtoUser.objects.get(id=5).name,
+        IDtoUser.objects.get(id=6).ID_Device:IDtoUser.objects.get(id=6).name,}
+    ExitandEnter = {'user.exited_geofence': 'exited', 'user.entered_geofence': 'entered'}
+
     if (lengthofEvents > NumberofEvents.objects.get(id=1).number):
 
     	t = NumberofEvents.objects.get(id=1)
@@ -21,7 +29,7 @@ def index(request):
     	t.number = lengthofEvents
     	t.save()
     	for i in range(difference):
-    		send_mail(event_details['events'][i]['user']['_id'], event_details['events'][i]['user']['_id']+ ' ' + event_details['events'][i]['type'] + ' ' + event_details['events'][i]['geofence']['description'],'anupriyamranjit8@gmail.com',['nafap19986@topmail1.net'],fail_silently=False)
+    		send_mail(ID_DICT.get(event_details['events'][i]['user']['_id']), ID_DICT.get(event_details['events'][i]['user']['_id'])+ ' ' + ExitandEnter.get(event_details['events'][i]['type']) + ' ' + event_details['events'][i]['geofence']['description'],'anupriyamranjit8@gmail.com',['nafap19986@topmail1.net'],fail_silently=False)
     else:
     	pass
 
@@ -38,9 +46,8 @@ def index(request):
             'Employee_Location': Location_Value,
             'Accuracy': employee_location['user']['locationAccuracy'],
             'Last_Update': employee_location['user']['actualUpdatedAt'],
-            'Coordinate' : [round(employee_location['user']['location']['coordinates'][0], 3),round(employee_location['user']['location']['coordinates'][1], 3)],
+            'Coordinate' : [round(employee_location['user']['location']['coordinates'][0], 5),round(employee_location['user']['location']['coordinates'][1], 5)],
             'TypeofPhone' : employee_location['user']['deviceType'],
-            'PrintStatement' : lengthofEvents
             }
         employeelocation.append(employee)
     context = {'employeelocation' : employeelocation }
